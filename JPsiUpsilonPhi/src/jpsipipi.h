3ec82d8ce22ada2d554ab65c7c1e7b26d300fec3
cobby319 <allenwang@buaa.edu.cn>
2018-07-03 20:32:57 +0800

    add new final states

---
+++ JPsiUpsilonPhi/src/jpsipipi.h
@@ -0,0 +1,132 @@
+#ifndef _miniAODmuons_h
+#define _miniAODmuons_h
+
+// system include files
+#include <memory>
+
+// user include files
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "DataFormats/Common/interface/Handle.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
+
+#include "RecoVertex/KinematicFit/interface/KinematicParticleVertexFitter.h"
+#include "RecoVertex/KinematicFit/interface/KinematicParticleFitter.h"
+#include "RecoVertex/KinematicFit/interface/MassKinematicConstraint.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/KinematicParticle.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/RefCountedKinematicParticle.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/TransientTrackKinematicParticle.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/KinematicParticleFactoryFromTransientTrack.h"
+#include "RecoVertex/AdaptiveVertexFit/interface/AdaptiveVertexFitter.h"
+
+#include "TrackingTools/Records/interface/TrackingComponentsRecord.h"
+
+#include "TrackingTools/TransientTrack/interface/TransientTrackFromFTSFactory.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "TrackingTools/PatternTools/interface/ClosestApproachInRPhi.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "DataFormats/PatCandidates/interface/PackedCandidate.h" // muy importante para MiniAOD
+
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/Candidate/interface/CandidateFwd.h"
+#include "DataFormats/Candidate/interface/CompositeCandidate.h"
+#include "DataFormats/Candidate/interface/VertexCompositeCandidate.h"
+#include "DataFormats/V0Candidate/interface/V0Candidate.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
+
+#include "CondFormats/L1TObjects/interface/L1GtTriggerMenu.h"
+#include "CondFormats/DataRecord/interface/L1GtTriggerMenuRcd.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutSetupFwd.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMapRecord.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutSetup.h"
+#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
+
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/PatCandidates/interface/GenericParticle.h"
+
+#include "RecoVertex/VertexPrimitives/interface/BasicSingleVertexState.h"
+#include "RecoVertex/VertexPrimitives/interface/VertexState.h"
+
+#include "TFile.h"
+#include "TTree.h"
+
+
+//
+// class decleration
+//
+
+class miniAODmuons : public edm::EDAnalyzer {
+public:
+  explicit miniAODmuons(const edm::ParameterSet&);
+  ~miniAODmuons();
+  
+private:
+  virtual void beginJob() ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+  void printout(const RefCountedKinematicVertex& myVertex) const;
+  void printout(const RefCountedKinematicParticle& myParticle) const;
+  void printout(const RefCountedKinematicTree& myTree) const;
+
+    // ----------member data ---------------------------
+  
+  edm::EDGetTokenT<edm::View<pat::Muon>> dimuon_Label;
+  edm::EDGetTokenT<edm::View<pat::PackedCandidate>> trakCollection_label;
+  edm::EDGetTokenT<reco::VertexCollection> primaryVertices_Label;
+
+  bool isMC_;
+
+  TTree*      tree_;
+  
+  std::vector<float>       *mumC2;
+  std::vector<int>         *mumNHits, *mumNPHits; 
+  std::vector<float>       *mupC2;
+  std::vector<int>         *mupNHits, *mupNPHits;
+  std::vector<float>       *mumdxy, *mupdxy, *mumdz, *mupdz;
+  std::vector<float>       *muon_dca;
+
+  std::vector<bool>        *mu1soft, *mu2soft, *mu1tight, *mu2tight;  
+  std::vector<bool>        *mu1PF, *mu2PF, *mu1loose, *mu2loose;  
+ 
+  unsigned int             nU, nJ,nPhi;
+    
+  std::vector<float>       *J_mass, *J_px, *J_py, *J_pz, *J_energy;
+
+  std::vector<float>       *J_px1, *J_py1, *J_pz1;
+  std::vector<float>       *J_px2, *J_py2, *J_pz2;
+  std::vector<int>         *J_charge1, *J_charge2;
+
+  
+  std::vector<float>       *U_mumC2;
+  std::vector<int>         *U_mumNHits, *U_mumNPHits;
+  std::vector<float>       *U_mupC2;
+  std::vector<int>         *U_mupNHits, *U_mupNPHits;
+  std::vector<float>       *U_mumdxy, *U_mupdxy, *U_mumdz, *U_mupdz;
+  std::vector<float>       *U_muon_dca;
+
+  std::vector<bool>        *U_mu1soft, *U_mu2soft, *U_mu1tight, *U_mu2tight;
+  std::vector<bool>        *U_mu1PF, *U_mu2PF, *U_mu1loose, *U_mu2loose;
+  std::vector<float>       *U_mass, *U_px, *U_py, *U_pz, *U_energy;
+
+  std::vector<float>       *U_px1, *U_py1, *U_pz1;
+  std::vector<float>       *U_px2, *U_py2, *U_pz2;
+  std::vector<int>         *U_charge1, *U_charge2;
+  std::vector<float>       *Phi_mass, *Phi_px, *Phi_py, *Phi_pz, *Phi_energy;
+  //std::vector<int>         *N_pfcandidate;
+};
+#endif
