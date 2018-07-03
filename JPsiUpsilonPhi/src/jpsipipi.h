02c9e3aace1119e3298ab8dce1014bc2110f229a
cobby319 <allenwang@buaa.edu.cn>
2018-07-03 21:26:54 +0800

    jpsipipi update

--- JPsiUpsilonPhi/src/jpsipipi.h
+++ JPsiUpsilonPhi/src/jpsipipi.h
@@ -103,7 +103,7 @@ private:
   std::vector<bool>        *mu1soft, *mu2soft, *mu1tight, *mu2tight;  
   std::vector<bool>        *mu1PF, *mu2PF, *mu1loose, *mu2loose;  
  
-  unsigned int             nU, nJ,nPhi;
+  unsigned int             nJ;
     
   std::vector<float>       *J_mass, *J_px, *J_py, *J_pz, *J_energy;
 
@@ -111,22 +111,5 @@ private:
   std::vector<float>       *J_px2, *J_py2, *J_pz2;
   std::vector<int>         *J_charge1, *J_charge2;
 
-  
-  std::vector<float>       *U_mumC2;
-  std::vector<int>         *U_mumNHits, *U_mumNPHits;
-  std::vector<float>       *U_mupC2;
-  std::vector<int>         *U_mupNHits, *U_mupNPHits;
-  std::vector<float>       *U_mumdxy, *U_mupdxy, *U_mumdz, *U_mupdz;
-  std::vector<float>       *U_muon_dca;
-
-  std::vector<bool>        *U_mu1soft, *U_mu2soft, *U_mu1tight, *U_mu2tight;
-  std::vector<bool>        *U_mu1PF, *U_mu2PF, *U_mu1loose, *U_mu2loose;
-  std::vector<float>       *U_mass, *U_px, *U_py, *U_pz, *U_energy;
-
-  std::vector<float>       *U_px1, *U_py1, *U_pz1;
-  std::vector<float>       *U_px2, *U_py2, *U_pz2;
-  std::vector<int>         *U_charge1, *U_charge2;
-  std::vector<float>       *Phi_mass, *Phi_px, *Phi_py, *Phi_pz, *Phi_energy;
-  //std::vector<int>         *N_pfcandidate;
 };
 #endif
