9e425ca6fc0558c7c34c372cbb3318220b620f25
cobby319 <allenwang@buaa.edu.cn>
2018-07-10 17:59:56 +0800

    charge update

--- JPsiUpsilonPhi/src/jpsipipi.cc
+++ JPsiUpsilonPhi/src/jpsipipi.cc
@@ -434,7 +434,7 @@ for(unsigned int i=0; i<JpsiFTS.size(); i++)
 	    if (JpsiPi_dxy/JpsiPi_dxyerr<3.0) continue;
 	    for(View<pat::PackedCandidate>::const_iterator iTrack2= iTrack1+1; iTrack2 != thePATTrackHandle->end();++iTrack2)
 	    {
-            if(!( (iTrack2->charge() )*( iTrack2->charge() )>0)) continue;
+            if(!( (iTrack2->charge() )*( iTrack2->charge() )<0)) continue;
             if(iTrack2->pt()<2.5)continue;
   	        if(iTrack2->eta()>2||iTrack1->eta()<-2)continue;
   	        
