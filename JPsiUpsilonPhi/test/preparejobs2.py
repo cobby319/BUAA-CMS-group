725560966bcae009660ffa87491b0d7f3f49396a
Hanwen <allenwang@buaa.edu.cn>
2018-09-20 16:16:06 +0200

    update script

--- JPsiUpsilonPhi/test/preparejobs2.py
+++ JPsiUpsilonPhi/test/preparejobs2.py
@@ -3,15 +3,16 @@ import re
 import os
 
 def printPath(path):  
-    dirname = os.getcwd()
     if not os.path.exists("OUTPUTS/"+type+"/JOBS/catalog_"+type+".txt"): 
         os.mknod("OUTPUTS/"+type+"/JOBS/catalog_"+type+".txt")
     file=open("OUTPUTS/"+type+"/JOBS/catalog_"+type+".txt",'a')
-    for dirs in os.listdir(path):
-        for fs in os.listdir(path+'/'+dirs):
-            if (os.path.isfile(path+'/'+dirs+'/'+fs)):
-                file.write('dcap://maite.iihe.ac.be'+path+'/'+dirs+'/'+fs)
+    for file in os.listdir(path):
+        if os.path.isfile(path+'/'+file):
+            if '.root' in file:
+                file.write('dcap://maite.iihe.ac.be'+path+'/'+file)
                 file.write("\n")
+        else :
+            printPath(path+'/'+file)
     file.close()
 
 
@@ -67,59 +68,8 @@ def main():
         os.makedirs('OUTPUTS/'+type+'/MERGED')
     if not os.path.exists('OUTPUTS/'+type+'/CONFIGS'):
         os.makedirs('OUTPUTS/'+type+'/CONFIGS')
-    if type == "mc_ele":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8-001-Ele/CRAB_PrivateMC/crab_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8-TagProbePruner-Ele/180408_045629')
-    if type == "mc_mu":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8-001/CRAB_PrivateMC/crab_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8-TagProbePruner-Mu/180408_034333')    
-    if type == "single_ele":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/SingleElectron001/CRAB_PrivateMC/crab_SingleElectron-TagProbePruner-Ele/180501_034315')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/SingleElectron002/CRAB_PrivateMC/crab_SingleElectron-TagProbePruner-Ele/180501_034717')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/SingleElectron003/CRAB_PrivateMC/crab_SingleElectron-TagProbePruner-Ele/180501_044954')
-    if type == "single_mu":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/SingleMuon-001/CRAB_PrivateMC/crab_SingleMuon-TagProbePruner-Mu/180408_042749')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/SingleMuon-002/CRAB_PrivateMC/crab_SingleMuon-TagProbePruner-Mu/180408_042911') 
-        printPath('/pnfs/iihe/cms/store/user/hanwen/SingleMuon-003/CRAB_PrivateMC/crab_SingleMuon-TagProbePruner-Mu/180408_042947')
-    if type == "DoubleMuon_Run2016":
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016B/DoubleMuon/MINIAOD/03Feb2017_ver2-v2')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016C/DoubleMuon/MINIAOD/03Feb2017-v1')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016D/DoubleMuon/MINIAOD/03Feb2017-v1')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016E/DoubleMuon/MINIAOD/03Feb2017-v1')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016F/DoubleMuon/MINIAOD/03Feb2017-v1')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016G/DoubleMuon/MINIAOD/03Feb2017-v1')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016H/DoubleMuon/MINIAOD/03Feb2017_ver3-v1')
-    if type == "Charm_Run2016":
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016E/Charmonium/MINIAOD/23Sep2016-v1')
-        printPath('/pnfs/iihe/cms/ph/sc4/store/data/Run2016H/Charmonium/MINIAOD/03Feb2017_ver3-v1')
-    if type == "Run2016":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016B/180714_053551')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016C/180714_053542')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016D/180714_053528')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016E/180714_053518')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016F/180714_053509')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016G/180714_053500')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/test20180715/crab_Charmonium_Run2016H/180714_053438')
-    if type == "Run2016B":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016B-03Feb2017_ver2-v2/180714_004045')
-    if type == "Run2016C":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016C-03Feb2017-v1/180714_004026')
-    if type == "Run2016D":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016D-03Feb2017-v1/180714_004054')       
-    if type == "Run2016E":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016E-03Feb2017-v1/180714_004007')   
-    if type == "Run2016F":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016F-03Feb2017-v1/180714_004017')
-    if type == "Run2016G":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016G-03Feb2017-v1/180714_004036')
-    if type == "Run2016H":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_Charmonium_Run2016H-03Feb2017_ver3-v1/180713_140833')
     if type == "test":
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016B/180728_062419')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016C/180728_062427')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016D/180728_062434')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016E/180728_062442')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016F/180728_062450')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016G/180728_062457')
-        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium/crab_JPsiPiPi_Run2016H/180728_062537')
+        printPath('/pnfs/iihe/cms/store/user/hanwen/Charmonium')
     jobsDirectory ='OUTPUTS/'+type+'/JOBS' 
     cfgDirectory = 'OUTPUTS/'+type +'/CONFIGS' 
     datasetFile = open("OUTPUTS/"+type+"/JOBS/catalog_"+type+".txt",'r')
