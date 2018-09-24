aff6656a1e8b616639a5893bad59bc2721c33eb3
Hanwen <allenwang@buaa.edu.cn>
2018-09-24 13:57:54 +0200

    update for 94X

---
+++ JPsiUpsilonPhi/test/jpsipipi/multicrab_data_Run2017B-F.py
@@ -0,0 +1,93 @@
+##########################
+#                        #
+#  2017-01-16 MC         #
+#                        #
+##########################
+dataset = {
+#'DoubleEG_Run2016B-03Feb2017-v2_final_resub'  : '/DoubleEG/Run2016B-03Feb2017_ver2-v2/MINIAOD',
+#'DoubleEG_Run2016C-03Feb2017-v1_final'  : '/DoubleEG/Run2016C-03Feb2017-v1/MINIAOD',
+#'DoubleEG_Run2016D-03Feb2017-v1_final_resub'  : '/DoubleEG/Run2016D-03Feb2017-v1/MINIAOD',           
+#'DoubleEG_Run2016E-03Feb2017-v1_final_resub'  : '/DoubleEG/Run2016E-03Feb2017-v1/MINIAOD',            
+#'DoubleEG_Run2016F-03Feb2017-v1_final'  : '/DoubleEG/Run2016F-03Feb2017-v1/MINIAOD',            
+#'DoubleEG_Run2016G-03Feb2017-v1_final_resub'  : '/DoubleEG/Run2016G-03Feb2017-v1/MINIAOD',            
+#'DoubleEG_Run2016H-03Feb2017-v2_final_resub'  : '/DoubleEG/Run2016H-03Feb2017_ver2-v1/MINIAOD',       
+#'DoubleEG_Run2016H-03Feb2017-v3_final'  : '/DoubleEG/Run2016H-03Feb2017_ver3-v1/MINIAOD',
+#'SingleElectron_Run2016B-03Feb2017-v2_final_resub': '/SingleElectron/Run2016B-03Feb2017_ver2-v2/MINIAOD',
+#'SingleElectron_Run2016C-03Feb2017-v1_final': '/SingleElectron/Run2016C-03Feb2017-v1/MINIAOD',
+#'SingleElectron_Run2016D-03Feb2017-v1_final_resub': '/SingleElectron/Run2016D-03Feb2017-v1/MINIAOD',
+#'SingleElectron_Run2016E-03Feb2017-v1_final_resub': '/SingleElectron/Run2016E-03Feb2017-v1/MINIAOD',
+#'SingleElectron_Run2016F-03Feb2017-v1_final': '/SingleElectron/Run2016F-03Feb2017-v1/MINIAOD',
+#'SingleElectron_Run2016G-03Feb2017-v1_final_resub': '/SingleElectron/Run2016G-03Feb2017-v1/MINIAOD',
+#'SingleElectron_Run2016H-03Feb2017-v2_final_resub': '/SingleElectron/Run2016H-03Feb2017_ver2-v1/MINIAOD',
+#'SingleElectron_Run2016H-03Feb2017-v3_final': '/SingleElectron/Run2016H-03Feb2017_ver3-v1/MINIAOD',
+#'MET_Run2016B-03Feb2017-v2_0510': '/MET/Run2016B-03Feb2017_ver2-v2/MINIAOD',
+#'MET_Run2016C-03Feb2017-v1_0510': '/MET/Run2016C-03Feb2017-v1/MINIAOD',
+#'MET_Run2016D-03Feb2017-v1_0510': '/MET/Run2016D-03Feb2017-v1/MINIAOD',
+#'MET_Run2016E-03Feb2017-v1_0510': '/MET/Run2016E-03Feb2017-v1/MINIAOD',
+#'MET_Run2016F-03Feb2017-v1_0510': '/MET/Run2016F-03Feb2017-v1/MINIAOD',
+#'MET_Run2016G-03Feb2017-v1_0510': '/MET/Run2016G-03Feb2017-v1/MINIAOD',
+#'MET_Run2016H-03Feb2017-v2_0510': '/MET/Run2016H-03Feb2017_ver2-v1/MINIAOD',
+#'MET_Run2016H-03Feb2017-v3_0510': '/MET/Run2016H-03Feb2017_ver3-v1/MINIAOD'
+#'SingleElectron_Run2016B-07Aug17':'/SingleElectron/Run2016B-07Aug17_ver2-v2/MINIAOD',
+#'SingleElectron_Run2016C-07Aug17':'/SingleElectron/Run2016C-07Aug17-v1/MINIAOD',
+#'SingleElectron_Run2016D-07Aug17':'/SingleElectron/Run2016D-07Aug17-v1/MINIAOD',
+#'SingleElectron_Run2016E-07Aug17':'/SingleElectron/Run2016E-07Aug17-v1/MINIAOD',
+#'SingleElectron_Run2016F-07Aug17':'/SingleElectron/Run2016F-07Aug17-v1/MINIAOD',
+#'SingleElectron_Run2016G-07Aug17':'/SingleElectron/Run2016G-07Aug17-v1/MINIAOD',
+#'SingleElectron_Run2016H-07Aug17':'/SingleElectron/Run2016H-07Aug17-v1/MINIAOD'
+#'DoubleEG_Run2016B-07Aug17':'/DoubleEG/Run2016B-07Aug17_ver2-v2/MINIAOD',
+#'DoubleEG_Run2016C-07Aug17':'/DoubleEG/Run2016C-07Aug17-v1/MINIAOD',
+#'DoubleEG_Run2016D-07Aug17':'/DoubleEG/Run2016D-07Aug17-v1/MINIAOD',
+#'DoubleEG_Run2016E-07Aug17':'/DoubleEG/Run2016E-07Aug17-v1/MINIAOD',
+#'DoubleEG_Run2016F-07Aug17':'/DoubleEG/Run2016F-07Aug17-v1/MINIAOD',
+#'DoubleEG_Run2016G-07Aug17':'/DoubleEG/Run2016G-07Aug17-v1/MINIAOD',
+#'DoubleEG_Run2016H-07Aug17':'/DoubleEG/Run2016H-07Aug17-v1/MINIAOD'
+'Charmonium_Run2017C':'/Charmonium/Run2017C-31Mar2018-v1/MINIAOD',
+'Charmonium_Run2017D':'/Charmonium/Run2017D-31Mar2018-v1/MINIAOD',
+'Charmonium_Run2017E':'/Charmonium/Run2017E-31Mar2018-v1/MINIAOD',
+'Charmonium_Run2017F':'/Charmonium/Run2017F-31Mar2018-v1/MINIAOD',
+'Charmonium_Run2017B':'/Charmonium/Run2017B-31Mar2018-v1/MINIAOD',
+#'Charmonium_Run2016H-03Feb2017_ver3-v1':'/Charmonium/Run2016H-03Feb2017_ver3-v1/MINIAOD'
+}
+
+nfiles = {
+}
+
+filesPerJob = {
+}
+
+if __name__ == '__main__':
+    from CRABAPI.RawCommand import crabCommand
+
+    def submit(config):
+        res = crabCommand('submit', config = config)
+
+    from CRABClient.UserUtilities import config
+    config = config()
+
+    name = '_data'
+    config.General.workArea = 'crab_Jpsipipi0924Run2017'
+    config.General.transferLogs = False
+    config.General.transferOutputs = True
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.psetName = 'test_2017B-F.py'
+  #  config.JobType.inputFiles   = ['data','rcdata.2016.v3']
+  #  config.JobType.pyCfgParams = ['DataProcessing=legacy_rerecodata']
+#    config.JobType.pyCfgParams = ['DataProcessing=rerecodata']
+#    config.JobType.pyCfgParams = ['DataProcessing=promptdata']
+    config.Data.inputDBS = 'global'
+    config.Data.splitting = 'LumiBased'
+    config.Data.unitsPerJob = 40000
+    config.Data.publication = False
+    config.Data.ignoreLocality = False
+    config.Data.lumiMask = 'Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt'
+#    config.Data.lumiMask = '/user/wenxing/FINAL_code_170407/CMSSW_8_0_26_patch1/src/UserCode/IIHETree/test/resubmit_DoubleEG.json'
+#    config.Data.lumiMask = '/user/wenxing/FINAL_code_170407/CMSSW_8_0_26_patch1/src/UserCode/IIHETree/test/resubmit_SingleEG.json'
+    config.Site.storageSite = 'T2_BE_IIHE'
+
+    for sample in dataset:
+        config.General.requestName = sample
+        config.Data.inputDataset = dataset[sample]
+#        config.Data.outputDatasetTag = sample
+        submit(config)
+
