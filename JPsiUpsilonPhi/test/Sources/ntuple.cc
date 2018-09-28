ef919d1ab534e8c659236117ec472f07f322e070
Hanwen <allenwang@buaa.edu.cn>
2018-09-28 11:56:15 +0200

    test

--- JPsiUpsilonPhi/test/Sources/ntuple.cc
+++ JPsiUpsilonPhi/test/Sources/ntuple.cc
@@ -58,7 +58,7 @@ void ntuple::Loop()
       if(jentry % 10000 ==0) cout << jentry << " of " << nentries << endl;
       double weight = 1.;
       mon.fillHisto("Num_JPsi","tot",nJ,weight);;
-
+      mon.fillHisto("Num_PiPi","tot",nPiPair,weight);;
       auto smallestchi2 = std::min_element(Pi_vertexchisq2->begin(), Pi_vertexchisq2->end());
       int piN =std::distance(Pi_vertexchisq2->begin(), smallestchi2);
       //cout <<  "piN  is " << piN << endl;
@@ -127,14 +127,26 @@ void ntuple::Loop()
       mon.fillHisto("Pi_dxyerr2","",Pi_dxyerr2->at(piN),weight); 
       mon.fillHisto("Pi_vertexchisq1","",Pi_vertexchisq1->at(piN),weight); 
       mon.fillHisto("Pi_vertexchisq2","",Pi_vertexchisq2->at(piN),weight); 
-      if(!mu1tight->at(jpsiN) || !mu2tight->at(jpsiN)) continue;
+      //if(!mu1tight->at(jpsiN) || !mu2tight->at(jpsiN)) continue;
+      if(!mu1loose->at(jpsiN) || !mu2loose->at(jpsiN)) continue;
+      if(Pi_vertexchisq1->at(piN) >6 ) continue;
+      if (Pi_vertexchisq12->at(piN)> 6+Pi_vertexchisq1->at(piN) ) continue;
+      if (J_lxy->at(jpsiN) <0.006) continue;
+      if (Pi_dxy1->at(piN) <0.006) continue;
+      if (Pi_dxy2->at(piN) <0.006) continue;
+      if (Pi_nhits1->at(piN) <10) continue;
+      if (Pi_nhits2->at(piN) <10) continue;
+
       TLorentzVector jpsi, pion1,pion2;
       float Pion_mass = 0.13957061;
       jpsi.SetXYZM(J_px->at(jpsiN),J_py->at(jpsiN),J_pz->at(jpsiN),J_mass->at(jpsiN)); 
-      pion1.SetPtEtaPhiM(Pi_pt1->at(piN),Pi_eta1->at(piN),Pi_phi1->at(piN),Pion_mass);
-      pion2.SetPtEtaPhiM(Pi_pt2->at(piN),Pi_eta2->at(piN),Pi_phi2->at(piN),Pion_mass);
+      pion1.SetPtEtaPhiE(Pi_pt1->at(piN),Pi_eta1->at(piN),Pi_phi1->at(piN),Pi_e1->at(piN));
+      pion2.SetPtEtaPhiE(Pi_pt2->at(piN),Pi_eta2->at(piN),Pi_phi2->at(piN),Pi_e2->at(piN));
+      //pion1.SetPtEtaPhiM(Pi_pt1->at(piN),Pi_eta1->at(piN),Pi_phi1->at(piN),Pion_mass);
+      //pion2.SetPtEtaPhiM(Pi_pt2->at(piN),Pi_eta2->at(piN),Pi_phi2->at(piN),Pion_mass);
       mon.fillHisto("M_JPsi","tot",jpsi.M(),weight);
       mon.fillHisto("pT_JPsi","tot",jpsi.Pt(),weight);
+      if (jpsi.Pt() <20) continue;
       mon.fillHisto("Deta JpsiPi1","tot",jpsi.Eta()-pion1.Eta(),weight); 
       mon.fillHisto("Deta JpsiPi2","tot",jpsi.Eta()-pion2.Eta(),weight); 
       mon.fillHisto("Dphi JpsiPi1","tot",jpsi.Phi()-pion1.Phi(),weight); 
@@ -145,11 +157,11 @@ void ntuple::Loop()
       float rx,ry,rz;
       px = (jpsi+pion1+pion2).Px();
       py = (jpsi+pion1+pion2).Py();
-      pz = (jpsi+pion1+pion2).Pz();
+      //pz = (jpsi+pion1+pion2).Pz();
       rx = JPiPi_x->at(piN);
       ry = JPiPi_y->at(piN);
-      rz = JPiPi_z->at(piN);
-      float cosine = (px*rx+py*ry+pz*rz)/((px*px+py*py+pz*pz)*(rx*rx+ry*ry+rz*rz));
+      //rz = JPiPi_z->at(piN);
+      float cosine = (px*rx+py*ry)/((px*px+py*py)*(rx*rx+ry*ry));
       mon.fillHisto("cosine of P&r","total",cosine,weight);
       float pipi_mass =(pion1+pion2).M();
       if (pipi_mass>0.81 && pipi_mass<0.97) continue;
