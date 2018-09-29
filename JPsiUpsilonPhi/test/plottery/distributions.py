9fc9a99bea64cdfea2d3a4c7e13e8302482a48be
Hanwen <allenwang@buaa.edu.cn>
2018-09-29 10:44:18 +0200

    improve distribution plots

--- JPsiUpsilonPhi/test/plottery/distributions.py
+++ JPsiUpsilonPhi/test/plottery/distributions.py
@@ -2,17 +2,29 @@ import ROOT as r
 import os
 import plottery as ply
 
-histos= {'pT_JPsi_':'J_pT','DeltaR_JpsiPi1_':'JP1_deltaR','DeltaR_JpsiPi2_':'JP2_deltaR','JPiPi_lxy_':'JPP_lxy'}
-file = r.TFile("../Merged.root")
+histos= {'pT_JPsi_tot':'pT_JPsi_final','DeltaR_JpsiPi1_':'DeltaR_JpsiPi1_final','DeltaR_JpsiPi2_':'DeltaR_JpsiPi2_final','JPiPi_lxy_':'JPiPi_lxy_final'}
+file = r.TFile("Merged.root")
 for hist in histos:
-    h= r.TH1F()
-    file.GetObject("histos/"+hist,h)
+    h1= r.TH1F()
+    h2= r.TH1F()
+    file.GetObject("histos/"+hist,h1)
+    file.GetObject("histos/"+histos[hist],h2)
     ply.plot_hist(
-        bgs=[h],
+        bgs=[h1],
+        legend_labels = ["before cut"],
+        sigs = [h2],
+        sig_labels = ["signal mass region"],
         options = {
+            "legend_scalex": 0.7,
+            "legend_scaley": 1.5,
+            "cms_label": "Preliminary",
+            "lumi_value": "-inf",
             "do_stack": False,
             "yaxis_log": True,
-            "output_name": "../distributions.pdf",
-            "output_ic": True,
+            "output_name": histos[hist]+".pdf",
+            "yaxis_range": [0.1,1000000000],
+            "yaxis_moreloglabels":False,
+            #"yaxis_noexponents":True,
+            #"output_ic": True,
             }
         )
