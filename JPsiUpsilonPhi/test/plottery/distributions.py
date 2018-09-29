7c335e66d5265a7a23a08afe582f656fbdc69a7d
cobby319 <allenwang@buaa.edu.cn>
2018-09-29 11:08:37 +0200

    update

--- JPsiUpsilonPhi/test/plottery/distributions.py
+++ JPsiUpsilonPhi/test/plottery/distributions.py
@@ -2,13 +2,15 @@ import ROOT as r
 import os
 import plottery as ply
 
-histos= {'pT_JPsi_tot':'pT_JPsi_final','DeltaR_JpsiPi1_':'DeltaR_JpsiPi1_final','DeltaR_JpsiPi2_':'DeltaR_JpsiPi2_final','JPiPi_lxy_':'JPiPi_lxy_final'}
+histos= ['pT_JPsi_','DeltaR_JpsiPi1_','DeltaR_JpsiPi2_','JPiPi_lxy_','M_JPsi','"Deta_JpsiPi1','Deta_JpsiPi2','Dphi_JpsiPi1','"Dphi_JpsiPi2','mumNHits','mumNPHits','mupNHits','mupNPHits','mu1soft','mu2soft','mu1tight','mu2tight','mu1loose','mu2loose','J_vertexchi2','Pi_nhits1','Pi_npixelhits1','Pi_nhits2','Pi_npixelhits2','Pi_vertexchisq1','Pi_vertexchisq2']
 file = r.TFile("Merged.root")
+if not os.path.exists('distribution_PLOTS'): 
+    os.makedirs('distribution_PLOTS')
 for hist in histos:
     h1= r.TH1F()
     h2= r.TH1F()
     file.GetObject("histos/"+hist,h1)
-    file.GetObject("histos/"+histos[hist],h2)
+    file.GetObject("histos/"+hist+"final",h2)
     ply.plot_hist(
         bgs=[h1],
         legend_labels = ["before cut"],
@@ -21,7 +23,7 @@ for hist in histos:
             "lumi_value": "-inf",
             "do_stack": False,
             "yaxis_log": True,
-            "output_name": histos[hist]+".pdf",
+            "output_name": "distribution_PLOTS/test0929_"+hist+".pdf",
             "yaxis_range": [0.1,1000000000],
             "yaxis_moreloglabels":False,
             #"yaxis_noexponents":True,
