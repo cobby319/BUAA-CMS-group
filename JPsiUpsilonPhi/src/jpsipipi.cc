3f5fd2953408828871487e76fc9558de9f63655f
Hanwen <allenwang@buaa.edu.cn>
2018-10-05 16:19:44 +0200

    rm cosine cut in ntuple production

--- JPsiUpsilonPhi/src/jpsipipi.cc
+++ JPsiUpsilonPhi/src/jpsipipi.cc
@@ -614,7 +614,7 @@ for(unsigned int i=0; i<JpsiFTS.size(); i++)
 	        ry = psi_vFit_vertex_noMC2->position().y()-bestVtx.y();
 	        rz = psi_vFit_vertex_noMC2->position().z()-bestVtx.z();
 	        //float cosine = (px*rx+py*ry+pz*rz)/((px*px+py*py+pz*pz)*(rx*rx+ry*ry+rz*rz));
-	        float cosine = (px*rx+py*ry)/((px*px+py*py)*(rx*rx+ry*ry));
+	        //float cosine = (px*rx+py*ry)/((px*px+py*py)*(rx*rx+ry*ry));
 	        //if (cosine < 0.9 ) continue;
             float JpsiPiPi_dxy = TMath::Sqrt(rx*rx+ry*ry);
 	        float JpsiPiPi_dxyerr = psi_vFit_vertex_noMC2->error().rerr(pvertex);
