3d05d81b086009df0e711a62c03a0d8c085d8164
Hanwen <allenwang@buaa.edu.cn>
2018-10-24 12:02:11 +0200

    update

--- JPsiUpsilonPhi/test/jpsipipi/multicrab_MC.py
+++ JPsiUpsilonPhi/test/jpsipipi/multicrab_MC.py
@@ -4,6 +4,7 @@
 #                        #
 ##########################
 dataset = {
+'/BcToJPsiBcPt8Y2p5_MuNoCut_13TeV-bcvegpy2-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM'
 }
 
 nfiles = {
@@ -21,23 +22,28 @@ if __name__ == '__main__':
     from CRABClient.UserUtilities import config
     config = config()
 
-    name = '_mc'
-    config.General.workArea = 'crab_projects_TTsys_20180426'
+    name = '_data'
+    config.General.workArea = 'crab_MC'
     config.General.transferLogs = False
     config.General.transferOutputs = True
     config.JobType.pluginName = 'Analysis'
-    config.JobType.psetName = 'IIHE.py'
-#    config.JobType.psetName = 'IIHE_PL.py'
+    config.JobType.psetName = 'test_MC.py'
     config.JobType.pyCfgParams = ['DataProcessing=mc2016']
     config.JobType.inputFiles   = ['data','rcdata.2016.v3']
+  #  config.JobType.inputFiles   = ['data','rcdata.2016.v3']
+  #  config.JobType.pyCfgParams = ['DataProcessing=legacy_rerecodata']
+#    config.JobType.pyCfgParams = ['DataProcessing=rerecodata']
+#    config.JobType.pyCfgParams = ['DataProcessing=promptdata']
     config.Data.inputDBS = 'global'
-    #config.Data.splitting = 'FileBased'
-    #config.Data.unitsPerJob = 1
+    #config.Data.splitting = 'Automatic'
     config.Data.splitting = 'EventAwareLumiBased'
     config.Data.unitsPerJob = 20000
     config.Data.publication = False
     config.Data.ignoreLocality = False
-    config.Data.outLFNDirBase = '/store/user/wenxing/TTbar_sys_ext_sample_2016'
+    config.Data.outLFNDirBase = '/store/user/hanwen/BcToJPsiBcPt8Y2p5'
+ #   config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Final/Cert_271036-284044_13TeV_PromptReco_Collisions16_JSON.txt'
+#    config.Data.lumiMask = '/user/wenxing/FINAL_code_170407/CMSSW_8_0_26_patch1/src/UserCode/IIHETree/test/resubmit_DoubleEG.json'
+#    config.Data.lumiMask = '/user/wenxing/FINAL_code_170407/CMSSW_8_0_26_patch1/src/UserCode/IIHETree/test/resubmit_SingleEG.json'
     config.Site.storageSite = 'T2_BE_IIHE'
 
     for sample in dataset:
---
+++ JPsiUpsilonPhi/test/jpsipipi/test_MC.py
@@ -0,0 +1,66 @@
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Rootuple")
+
+process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+#process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+#process.GlobalTag = GlobalTag(process.GlobalTag, '94X_dataRun2_v6', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc')
+#process.GlobalTag = GlobalTag(process.GlobalTag, '80X_dataRun2_Prompt_v16', '')
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options.allowUnscheduled = cms.untracked.bool(True)
+#process.load("FWCore.MessageLogger.MessageLogger_cfi")
+#process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))#,SkipEvent = cms.untracked.vstring('ProductNotFound'))
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+
+#'/store/data/Run2017B/Charmonium/MINIAOD/PromptReco-v1/000/297/050/00000/183B4680-3356-E711-B33A-02163E014487.root',
+'/store/data/Run2016H/Charmonium/MINIAOD/03Feb2017_ver3-v1/100000/285996B8-64EC-E611-BF01-008CFAFBEFE8.root',
+ )
+)
+
+#process.load("myAnalyzers.JPsiKsPAT.miniAODmuonsRootupler_cfi")
+#process.rootuple.dimuons = cms.InputTag('miniaodPATMuonsWithTrigger') 
+process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
+                                        triggerConditions = cms.vstring('HLT_Dimuon20_Jpsi_v*',
+                                                                        'HLT_Dimuon16_Jpsi_v*',
+                                                                        'HLT_DoubleMu4_3_Jpsi_Displaced_v*',
+                                                                        'HLT_DoubleMu4_JpsiTrk_Displaced_v*',
+                                                                       # 'HLT_DoubleMu4_Jpsi_Displaced_v*'                                   
+                                                                       ),
+                                        hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+                                        l1tResults = cms.InputTag( "" ),
+                                        throw = cms.bool(False)
+                                        )
+process.load("BUAA-CMS-group.JPsiUpsilonPhi.slimmedMuonsTriggerMatcher2016_cfi")
+
+process.rootuple = cms.EDAnalyzer('jpsipipi',
+                          dimuons = cms.InputTag("slimmedMuons"),
+                          Trak = cms.InputTag("packedPFCandidates"),
+                          primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                          isMC = cms.bool(False),
+                          )
+process.rootuple.dimuons = cms.InputTag('slimmedMuonsWithTrigger')
+
+process.TFileService = cms.Service("TFileService",
+       #fileName = cms.string('Rootuple_Jpsi_2017-MiniAOD.root'),
+       fileName = cms.string('Rootuple_JpsiPiPi.root'),
+)
+
+process.mySequence = cms.Sequence(
+                                   process.triggerSelection *
+                                   process.slimmedMuonsWithTriggerSequence *
+                                   process.rootuple
+                                   )
+process.p = cms.Path(process.triggerSelection*process.slimmedMuonsWithTriggerSequence*process.rootuple)
